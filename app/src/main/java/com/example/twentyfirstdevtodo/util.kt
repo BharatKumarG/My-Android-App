f30075d544b8e28c6eff98b30150fcc73925a067
BharatKumarG <bharathkumarguduru443@gmail.com>
2025-08-28 09:47:53 +0530

    change this to v2

--- app/src/main/java/com/example/twentyfirstdevtodo/util.kt
+++
@@ -1,9 +0,0 @@
-package com.example.twentyfirstdevtodo
-
-import java.text.SimpleDateFormat
-import java.util.*
-
-fun formatDate(millis: Long): String { 
-    val sdf = SimpleDateFormat("MMM d, yyyy", Locale.getDefault())
-    return sdf.format(Date(millis))
-}

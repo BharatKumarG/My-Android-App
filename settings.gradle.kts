15a23c7c712ffb68c5847fe7c620c354f61d8b8e
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-08-28 09:51:15 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,16 @@
-rootProject.name = "MyAndroidAppEnhanced"
+pluginManagement {
+    repositories {
+        google()          // 👈 Required for Android Gradle Plugin
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "My-Android-App"
 include(":app")
